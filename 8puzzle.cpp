dc96d4770386b44a8c59713cc4c93a4155b92bb1
Aithani24 <gauravaithani7@gmail.com>
2025-08-31 14:12:41 +0530

    Update 8puzzle DFS.cpp

--- 8puzzle.cpp
+++ 8puzzle.cpp
@@ -1,132 +1,75 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <queue>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-typedef vector<vector<int>> Matrix;
-typedef vector<int> Coord;
-
-void displayBoard(Matrix board) {
-    for (int i = 0; i < board.size(); i++) {
-        for (int j = 0; j < board[0].size(); j++) {
-            cout << board[i][j] << " ";
+// Display the puzzle board
+void showBoard(vector<vector<int>>& state) {
+    for (int r = 0; r < 3; r++) {
+        for (int c = 0; c < 3; c++) {
+            cout << state[r][c] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
+    cout << "------" << endl;
 }
 
-bool isSameBoard(Matrix a, Matrix b) {
-    for (int r = 0; r < a.size(); r++) {
-        for (int c = 0; c < a[0].size(); c++) {
-            if (a[r][c] != b[r][c])
-                return false;
+// Check if current state is equal to target state
+bool isTarget(vector<vector<int>>& curr, vector<vector<int>>& target) {
+    return curr == target;
+}
+
+// Locate position of blank tile (0)
+pair<int,int> locateBlank(vector<vector<int>>& state) {
+    for (int r = 0; r < 3; r++) {
+        for (int c = 0; c < 3; c++) {
+            if (state[r][c] == 0) return {r,c};
         }
     }
-    return true;
+    return {-1,-1}; 
 }
 
-Matrix swapWithZero(Matrix board, Coord zeroPos, Coord swapPos) {
-    int temp = board[swapPos[0]][swapPos[1]];
-    board[swapPos[0]][swapPos[1]] = 0;
-    board[zeroPos[0]][zeroPos[1]] = temp;
-    return board;
-}
+// DFS search
+bool depthFirstSearch(vector<vector<int>> start, vector<vector<int>> target) {
+    stack<vector<vector<int>>> stk;
+    set<vector<vector<int>>> seen;
+    stk.push(start);
 
-Coord locateZero(Matrix board) {
-    Coord pos = {-1, -1};
-    for (int r = 0; r < board.size(); r++) {
-        for (int c = 0; c < board[0].size(); c++) {
-            if (board[r][c] == 0) {
-                pos[0] = r;
-                pos[1] = c;
-                break;
+    // possible moves: down, up, right, left
+    vector<pair<int,int>> moves = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    while (!stk.empty()) {
+        auto node = stk.top();
+        stk.pop();
+
+        if (isTarget(node, target)) return true;
+        if (seen.count(node)) continue;
+        seen.insert(node);
+
+        auto [br, bc] = locateBlank(node);
+
+        for (auto mv : moves) {
+            int nr = br + mv.first;
+            int nc = bc + mv.second;
+            if (nr >= 0 && nr < 3 && nc >= 0 && nc < 3) {
+                auto newState = node;
+                swap(newState[nr][nc], newState[br][bc]);
+                if (!seen.count(newState)) stk.push(newState);
             }
         }
-    }
-    return pos;
-}
-
-vector<Matrix> getNextStates(Matrix board) {
-    Coord zero = locateZero(board);
-    vector<Matrix> kids;
-
-    int row = zero[0], col = zero[1];
-
-    if (row > 0) {
-        kids.push_back(swapWithZero(board, zero, {row - 1, col}));
-    }
-
-    if (row < board.size() - 1) {
-        kids.push_back(swapWithZero(board, zero, {row + 1, col}));
-    }
-
-    if (col > 0) {
-        kids.push_back(swapWithZero(board, zero, {row, col - 1}));
-    }
-
-    if (col < board[0].size() - 1) {
-        kids.push_back(swapWithZero(board, zero, {row, col + 1}));
-    }
-
-    return kids;
-}
-
-bool alreadySeen(Matrix board, vector<Matrix>& visited) {
-    for (auto& b : visited) {
-        if (isSameBoard(b, board)) return true;
     }
     return false;
 }
 
-void runDFS(Matrix start, Matrix goal) {
-    stack<Matrix> nodes;
-    vector<Matrix> visited;
-
-    nodes.push(start);
-
-    while (!nodes.empty()) {
-        Matrix current = nodes.top();
-        nodes.pop();
-
-        if (isSameBoard(current, goal)) {
-            cout << "Solution found!!\n";
-            displayBoard(current);
-            return;
-        }
-
-        visited.push_back(current);
-
-        vector<Matrix> children = getNextStates(current);
-        for (auto& child : children) {
-            if (!alreadySeen(child, visited)) {
-                nodes.push(child);
-            }
-        }
-    }
-
-    cout << "No possible Solution found\n";
-}
-
-void runBFS(Matrix start, Matrix goal) {
-    queue<Matrix> nodeQueue;
-}
-
 int main() {
-    Matrix goalState = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 0}
-    };
+    vector<vector<int>> startState = {{1,2,3},{4,5,6},{0,7,8}};
+    vector<vector<int>> goalState  = {{1,2,3},{4,5,6},{7,8,0}};
 
-    Matrix startState = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 0, 8}
-    };
+    cout << "Initial Configuration:\n";
+    showBoard(startState);
 
-    runDFS(startState, goalState);
+    if (depthFirstSearch(startState, goalState)) 
+        cout << "Solution Found!\n";
+    else 
+        cout << "No Solution Exists!\n";
 
     return 0;
 }
